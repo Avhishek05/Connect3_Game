15ab4a8d2799803d0549657fc77dba54c9db9239
Amit Sirohiya <amit.sirohiya@essilor.com.sg>
2019-08-22 11:24:02 +0530

    Connect 3 Game created

---
+++ app/src/main/java/com/example/connect3/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.connect3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.gridlayout.widget.GridLayout;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    //0: yellow, 1: red, 3: empty
+    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+    int activePlayer = 0;
+    int[][] winningPosition = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
+    String winner;
+    boolean gameActive=true;
+    public void dropIn(View view) {
+        ImageView counter = (ImageView) view;
+        int tappedCounter = Integer.parseInt(counter.getTag().toString());
+        if (gameState[tappedCounter] == 2  && gameActive) {
+            gameState[tappedCounter] = activePlayer;
+            counter.setTranslationY(-1500);
+            if (activePlayer == 0) {
+                activePlayer = 1;
+                counter.setImageResource(R.drawable.yellow);
+            } else {
+                activePlayer = 0;
+                counter.setImageResource(R.drawable.red);
+            }
+            counter.animate().translationYBy(1500).setDuration(300);
+            for (int[] winningPosition : winningPosition) {
+                if (gameState[winningPosition[0]] == gameState[winningPosition[1]]
+                        && gameState[winningPosition[1]] == gameState[winningPosition[2]]
+                        && gameState[winningPosition[0]] != 2) {
+                    gameActive=false;
+                    if (activePlayer == 1) {
+                        winner = "Yellow";
+                    } else {
+                        winner = "Red";
+                    }
+                    Button playAgainButton = findViewById(R.id.playAgainButton);
+                    TextView winnerTextView = findViewById(R.id.winnerTextView);
+                    winnerTextView.setText(winner + " Won");
+                    playAgainButton.setVisibility(view.VISIBLE);
+                    winnerTextView.setVisibility(view.VISIBLE);
+//                    Toast.makeText(this, winner + " won", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    public void playAgain(View view){
+        Button playAgainButton = findViewById(R.id.playAgainButton);
+        TextView winnerTextView = findViewById(R.id.winnerTextView);
+        playAgainButton.setVisibility(view.INVISIBLE);
+        winnerTextView.setVisibility(view.INVISIBLE);
+        GridLayout gridLayout = (GridLayout) findViewById(R.id.gridLayout);
+
+        for(int i=0 ; i<gameState.length;i++)
+            gameState[i] = 2;
+        activePlayer = 0;
+        gameActive=true;
+        for(int i = 0; i< gridLayout.getChildCount();i++)
+        {
+            ImageView counter = (ImageView) gridLayout.getChildAt(i);
+            counter.setImageDrawable(null);
+        }
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
